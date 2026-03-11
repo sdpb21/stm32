ef6b9847526d67fc574a232d02ff684aa41b21fa
sdpb21 <jytprf@gmail.com>
2026-03-11 16:12:47 -0300

    Access levels code completed to change from privileged access mode to unprivileged - March 11 2026 at 16:12

--- 004_access_levels/Src/main.c
+++ 004_access_levels/Src/main.c
@@ -17,13 +17,59 @@
  */
 
 #include <stdint.h>
+#include <stdio.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+// This function executes in THREAD MODE of the processor.
+void generate_interrupt()
+{
+	uint32_t *pSTIR  = (uint32_t *)0xE000EF00;
+	uint32_t *pISERO = (uint32_t *)0xE000E100;
+
+	// Enable IRQ3 interrupt
+	*pISERO |= (1 << 3);
+
+	// Generate an interrupt from software for IRQ3
+	*pSTIR = (3 & 0x1FF);
+
+}
+
+void change_access_level_unpriv(void){
+
+	// read
+	__asm volatile ("MRS R0,CONTROL");
+	// modify
+	__asm volatile ("ORR R0,R0,#0x01");
+	// write
+	__asm volatile ("MSR CONTROL,R0");
+
+}
+// This function executes in THREAD MODE of the processor.
 int main(void)
 {
+	printf("In thread mode: before interrupt\n");
+
+	change_access_level_unpriv();
+
+	generate_interrupt();
+
+	printf("In thread mode: after interrupt\n");
     /* Loop forever */
 	for(;;);
+}
+
+// This function (ISR) executes in HANDLER MODE of the processor.
+void RTC_WKUP_IRQHandler(void)
+{
+	printf("In handler mode: ISR\n");
+}
+
+void HardFault_Handler(void){
+
+	printf("Hard fault detected\n");
+	while(1);
+
 }
